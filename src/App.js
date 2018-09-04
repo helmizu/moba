c94135f1f294b1b18051da042c3f2a48ae798056
Muhammad Ilham Fajar <fajarmuhammadilham@gmail.com>
2018-09-04 12:06:29 +0700

    setting redux

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage'
 import Team from './components/LandingPage/Team'
+import Tes from './components/LandingPage/Tes'
+import { Provider } from 'react-redux'
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/team" component={Team} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Route exact path="/" component={Tes} />
+            <Route path="/team" component={Team} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
