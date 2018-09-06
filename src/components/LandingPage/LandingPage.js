24e33456cf5bd8ead47b485ad476771f0ab916fd
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2018-09-06 23:59:09 +0700

    set navbar

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -20,10 +20,7 @@ export default class LandingPage extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light landing">
                     <a className="navbar-brand" href="#"><img src={require("../../assets/img/ts-2.png")} width="15%" /></a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item activeNavLanding">
