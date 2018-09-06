ab4e60af7526c7a5ca0ccf78c1097bde998e974c
Muhammad Ilham Fajar <muhammad_fajar_25rpl@student.smktelkom-mlg.sch.id>
2018-09-06 22:26:37 +0700

    daftar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import { Provider } from 'react-redux'
 import store from './store';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage'
 import Team from './components/LandingPage/Team'
 import Tes from './components/LandingPage/Tes'
 import Dashboard from './components/Dashboard/Dashboard'
 import PrivateRoute from './components/PrivateRoute'
+import Daftar from './components/Daftar/Daftar';
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
             <Route exact path="/" component={LandingPage} />
             <Route path="/team" component={Team} />
             <Route path="/dashboard" component={Dashboard} />
+            <Route path="/daftar" component={Daftar} />
             <PrivateRoute path="/tes" component={Tes} />
           </div>
         </Router>
