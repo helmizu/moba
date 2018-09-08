ff7db264b5714b662dd6713942c6d74e481dd99a
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2018-09-08 20:44:16 +0700

    merge

--- src/components/Dashboard/DataTeam.js
+++ src/components/Dashboard/DataTeam.js
@@ -86,7 +86,7 @@ class DataTeam extends Component {
                             pemain.length ? 
                             (
                                 pemain.map(pem => { return (
-                                    <div className="comment-body" key={pem._id}>
+                                    <div className="comment-body data-pemain" onClick={() => this.props.modalToggle('pemain', true)} key={pem._id}>
                                         <div className="user-img"> <img src={pem.foto ? `${baseUrl}/images/${pem.foto}` : require('../../assets/img/2000px-Disc_Plain_grey.svg.png')} alt="pemain"
                                             className="img-circle" />
                                         </div>
@@ -101,7 +101,7 @@ class DataTeam extends Component {
                             ) 
                             : 
                             (
-                                <div className="comment-body">
+                                <div className="comment-body data-pemain" onClick={() => this.props.modalToggle('pemain', true)}>
                                     <div className="user-img"> <img src={require('../../assets/img/2000px-Disc_Plain_grey.svg.png')} alt="pelatih"
                                         className="img-circle" />
                                     </div>
